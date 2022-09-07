b1ac4bcaa88368c36ab95fbe369931584ec82c90
류현수 <neillie23@gmail.com>
2022-09-07 17:19:51 +0900

    BE_220906_Community API : 클래스 생성

---
+++ backend/src/main/java/com/ssafy/kirin/controller/CommunityController.java
@@ -0,0 +1,18 @@
+package com.ssafy.kirin.controller;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "커뮤니티 API",tags = {"커뮤니티 API"})
+@RestController
+@RequestMapping("/api/challenges")
+@RequiredArgsConstructor
+public class CommunityController {
+
+
+
+
+}
---
+++ backend/src/main/java/com/ssafy/kirin/repository/CommunityRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.kirin.repository;
+
+import com.ssafy.kirin.entity.Community;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommunityRepository extends JpaRepository<Community,Long> {
+
+
+
+}
