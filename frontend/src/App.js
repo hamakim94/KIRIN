dbf6e0f0191ed9f7a83e0d4b29a008932c9c20b2
이도엽 <lx460@naver.com>
2022-09-14 23:08:47 +0900

    FE_220914_starpage: pc페이지 생성 완료 및 스타페이지 진행중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { isMobile } from "react-device-detect";
 import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
@@ -6,21 +7,32 @@ import FlowPage from "./pages/FlowPage";
 import DonationPage from "./pages/DonationPage";
 import PlusPage from "./pages/PlusPage";
 import StarPage from "./pages/StarPage";
+import PcPage from "./pages/PcPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import SimpleBottomNavigation from "./components/common/SimpleBottomNavigation";
 function App() {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<HomePage></HomePage>}></Route>
-        <Route path='/search' element={<SearchPage></SearchPage>}></Route>
-        <Route path='/flow' element={<FlowPage></FlowPage>}></Route>
-        <Route path='/plus' element={<PlusPage></PlusPage>}></Route>
-        <Route path='/donation' element={<DonationPage></DonationPage>}></Route>
-        <Route path='/star/:starId' element={<StarPage></StarPage>}></Route>
-        <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
-        {/* <Route path='/mypage' element={<SearchPage></SearchPage>}></Route> */}
-      </Routes>
-    </div>
+    <>
+      {isMobile ? (
+        <div className='App'>
+          <Routes>
+            <Route path='/' element={<HomePage></HomePage>}></Route>
+            <Route path='/search' element={<SearchPage></SearchPage>}></Route>
+            <Route path='/flow' element={<FlowPage></FlowPage>}></Route>
+            <Route path='/plus' element={<PlusPage></PlusPage>}></Route>
+            <Route path='/donation' element={<DonationPage></DonationPage>}></Route>
+            <Route path='/star/:starId' element={<StarPage></StarPage>}></Route>
+            <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
+            {/* <Route path='/mypage' element={<SearchPage></SearchPage>}></Route> */}
+          </Routes>
+          <SimpleBottomNavigation></SimpleBottomNavigation>
+        </div>
+      ) : (
+        <div className='Pc'>
+          <PcPage></PcPage>
+        </div>
+      )}
+    </>
   );
 }
 
--- frontend/src/pages/StarPage.js
+++ frontend/src/pages/StarPage.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import styles from "./StarPage.module.css";
 
 function StarPage() {
-  return <div>StarPage</div>;
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: "#FFB200",
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={styles.wrapper}>
+        <div className={styles.topWrapper}>
+          <div className={styles.coverImg}></div> {/* 커버사진 */}
+          <div className={styles.starImg}></div> {/* 프로필사진 */}
+        </div>
+        <div style={styles}>
+          <span>기린기린</span>
+          <Button variant='contained' color='secondary'>
+            구독
+          </Button>
+        </div>{" "}
+        {/* 설명 */}
+        <div style={styles}></div> {/* 타이틀 */}
+        <div style={styles}></div> {/* 오른쪽 */}
+        <div style={styles}></div> {/* 챌린지카드 */}
+        <div style={styles}></div> {/* 타이틀 */}
+        <div style={styles}></div> {/* 커뮤니티 */}
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default StarPage;
