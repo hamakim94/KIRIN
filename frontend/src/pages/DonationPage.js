cffd60f86d978c29fab448e5e06760497ea16ecb
이보나 <bona17@naver.com>
2022-09-14 11:46:56 +0900

    FE_220914_donation: 페이지네이션 진행중

--- frontend/src/pages/DonationPage.js
+++ frontend/src/pages/DonationPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import DonationList from '../components/donation/DonationList';
 import styles from './DonationPage.module.css';
-import Pagination from '../components/donation/Pagination';
 
 function DonationPage() {
   return (
@@ -9,7 +8,6 @@ function DonationPage() {
       <div className={styles.pageTitle}>나의 기부</div>
       <DonationList styles={styles}></DonationList>
       <hr></hr>
-      <Pagination> </Pagination>
     </div>
   );
 }
