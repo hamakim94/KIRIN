19092b4dc4a1e996f154e7680f6b1d58aa1be569
류현수 <neillie23@gmail.com>
2022-09-19 23:58:09 +0900

    BE_220919_Notify : 푸시 알림 브랜치 생성 (진행 중)

---
+++ backend/src/main/java/com/ssafy/kirin/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.ssafy.kirin.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
+}
