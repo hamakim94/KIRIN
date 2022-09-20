5e3776b3b3249694b8b004b35c06155fc9f5d71a
류현수 <neillie23@gmail.com>
2022-09-20 23:37:21 +0900

    BE_220920_Notify : 푸시 알림 (진행 중)

--- backend/src/main/java/com/ssafy/kirin/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/kirin/config/WebSocketConfig.java
@@ -1,20 +1,33 @@
 package com.ssafy.kirin.config;
 
+import com.ssafy.kirin.handler.NotifyHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-
+    @Value("${domain.name}")
+    private final String domainName;
+    private final NotifyHandler notifyHandler;
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/user");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws").withSockJS();
 
     }
 }
