ea2dd01ac71012dfca1140ce513e611f389f05c6
이도엽 <lx460@naver.com>
2022-09-20 21:32:45 +0900

    Revert "Merge branch 'feat-frontend/my' into 'develop'"
    This reverts merge request !17

--- frontend/src/pages/PlusPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PlusPage() {
-  return <div>PlusPage</div>;
-}
-
-export default PlusPage;
--- frontend/src/pages/StarPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function StarPage() {
-  return <div>StarPage</div>;
-}
-
-export default StarPage;
