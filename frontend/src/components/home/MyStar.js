c6cd2e9cbde02dc2ef81b80ff61fe74e28c611bf
이도엽 <lx460@naver.com>
2022-09-20 18:15:13 +0900

    FE_220920_fix: 오탈자 수정

--- frontend/src/components/home/MyStar.js
+++ frontend/src/components/home/MyStar.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function MyStar(props) {
+  const num = 1;
   const navigate = useNavigate();
   return (
     <img
       alt='star'
       className={props.styles.starImg}
       src='https://cdn.pixabay.com/photo/2022/05/06/17/15/cartoon-giraffe-7178753_960_720.jpg'
-      onClick={() => navigate(`/star/3`)}
+      onClick={() => navigate(`/star/${num}`)}
     ></img>
   );
 }
