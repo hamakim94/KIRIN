e604a054f9b31d886c842cca827079dd822cce9b
류현수 <neillie23@gmail.com>
2022-09-22 11:25:47 +0900

    BE_220921_Swagger : JWT 토큰 헤더 추가 (완료)

--- backend/src/main/java/com/ssafy/kirin/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/kirin/config/SwaggerConfig.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,11 +19,30 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.OAS_30)
                 .useDefaultResponseMessages(false)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ssafy.kirin.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("ACCESSTOKEN", "JWT", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("ACCESSTOKEN", authorizationScopes));
     }
 
     private ApiInfo apiInfo(){
