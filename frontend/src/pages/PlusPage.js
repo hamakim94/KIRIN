0d6099c1e1e9ad3836b03fa7c97aae828cdacc23
이도엽 <lx460@naver.com>
2022-09-22 10:14:50 +0900

    FE_220922_challenge: 영상 등록 진행중

--- frontend/src/pages/PlusPage.js
+++ frontend/src/pages/PlusPage.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./PlusPage.module.css";
+import RecordRTC, { invokeSaveAsDialog } from "recordrtc";
 
 function PlusPage() {
-  return <div>PlusPage</div>;
+  const [stream, setStream] = useState(null);
+  const [blob, setBlob] = useState(null);
+  const refVideo = useRef(null);
+  const recorderRef = useRef(null);
+
+  const handleRecording = async () => {
+    // const cameraStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        width: 500,
+        height: 500,
+        frameRate: 30,
+      },
+      audio: false,
+    });
+
+    setStream(mediaStream);
+    recorderRef.current = new RecordRTC(mediaStream, { type: "video" });
+    recorderRef.current.startRecording();
+  };
+
+  const handleStop = () => {
+    recorderRef.current.stopRecording(() => {
+      setBlob(recorderRef.current.getBlob());
+    });
+  };
+
+  const handleSave = () => {
+    invokeSaveAsDialog(blob);
+  };
+
+  useEffect(() => {
+    if (!refVideo.current) {
+      return;
+    }
+
+    // refVideo.current.srcObject = stream;
+  }, [stream, refVideo]);
+  return (
+    <div className={styles.wrapper}>
+      <button onClick={handleRecording}>start</button>
+      <button onClick={handleStop}>stop</button>
+      <button onClick={handleSave}>save</button>
+      {blob && (
+        <video
+          src={URL.createObjectURL(blob)}
+          controls
+          autoPlay
+          ref={refVideo}
+          style={{ width: "100%" }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default PlusPage;
