f92963a4ecd954780ad450e1eebd04c61ee12b82
이승연 <seongyeon38@gmail.com>
2022-09-24 02:26:21 +0900

    BE_220924_mm webhook: mm webhook 관련 설정 완료

---
+++ backend/src/main/java/com/ssafy/kirin/webhook/MattermostProperties.java
@@ -0,0 +1,26 @@
+package com.ssafy.kirin.webhook;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties("notification.mattermost")
+@Primary
+public class MattermostProperties { // Application Properties에 등록한 값을 받아올 객체
+    private String channel;
+    private String pretext;
+    private String color = "#ff5d52";
+    private String authorName;
+    private String authorIcon;
+    private String title;
+    private String text = "";
+    private String footer = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+}
