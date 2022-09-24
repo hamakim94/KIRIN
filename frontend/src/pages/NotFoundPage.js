921918f47c6345d2e64ee06a9120bbe08ab27697
이도엽 <lx460@naver.com>
2022-09-24 22:54:51 +0900

    FE_220924_test: docker volume 테스트 진행중

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,10 +1,13 @@
 import React from "react";
-import WalletPage from "./WalletPage";
 
 function NotFoundPage() {
   return (
     <div>
-      <WalletPage></WalletPage>
+      <img
+        width={500}
+        height={500}
+        src='/data/e33c6d38-df5e-4859-942a-36214ecf5831KakaoTalk_20220924_220101828.jpg'
+      ></img>
     </div>
   );
 }
