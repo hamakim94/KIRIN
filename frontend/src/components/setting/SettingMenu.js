31087b399fda81de9467b27313b2b99c14cad756
이보나 <bona17@naver.com>
2022-09-26 23:28:26 +0900

    FE_220926_my: 로그아웃 진행중

--- frontend/src/components/setting/SettingMenu.js
+++ frontend/src/components/setting/SettingMenu.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import swal from 'sweetalert';
+import UseAxios from '../../utils/UseAxios';
 
 function SettingMenu(props) {
+  const [willLogout, setWillLogout] = useState([]);
+
+  const logout = () => {
+    swal({
+      text: '로그아웃 하시겠습니까?',
+      buttons: true,
+      dangerMode: true,
+    }).then((willLogout) => {
+      if (willLogout) {
+        UseAxios.get(`/users/logout`);
+        swal('로그아웃이 완료되었습니다.');
+      } else {
+      }
+      console.log('로그아웃');
+    });
+  };
+
   return (
     <div>
       <hr></hr>
@@ -24,7 +43,7 @@ function SettingMenu(props) {
         <div className={props.styles.menuName}>개인정보 처리방침</div>
       </a>
       <hr></hr>
-      <a href="">
+      <a onClick={logout}>
         <div className={props.styles.menuName}>로그아웃</div>
       </a>
       <hr></hr>
