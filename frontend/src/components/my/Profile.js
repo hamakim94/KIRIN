0b6cd31f6b1b68dc3c5bc23449d40a8fbd68e1c3
이도엽 <lx460@naver.com>
2022-09-27 18:42:25 +0900

    FE_220927_fix: 이미지 경로 수정

--- frontend/src/components/my/Profile.js
+++ frontend/src/components/my/Profile.js
@@ -5,7 +5,6 @@ import Context from '../../utils/Context';
 function Profile(props) {
   const navigate = useNavigate();
   const { userData } = useContext(Context);
-  console.log(userData);
   return (
     <div>
       {userData ? (
@@ -13,7 +12,7 @@ function Profile(props) {
           <img
             src={
               userData.profileImg
-                ? `/files/${userData.profileImg}`
+                ? `${process.env.REACT_APP_BASEURL}/files/${userData.profileImg}`
                 : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
             }
             className={props.styles.userImg}
