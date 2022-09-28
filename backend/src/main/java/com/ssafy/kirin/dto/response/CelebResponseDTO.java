ecc37cd8debeabb7ff940db59aa5a6ef79224f54
이승연 <seongyeon38@gmail.com>
2022-09-28 16:42:38 +0900

    BE_220928_user : user 관련 api test 및 수정 완료

---
+++ backend/src/main/java/com/ssafy/kirin/dto/response/CelebResponseDTO.java
@@ -0,0 +1,19 @@
+package com.ssafy.kirin.dto.response;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class CelebResponseDTO {
+    Long id;
+    String nickname;
+    String profileImg;
+    String info;
+    String coverImg;
+}
