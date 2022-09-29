1906a82c743aeea23d6a7a8c766cf863d0cd6e26
류현수 <neillie23@gmail.com>
2022-09-29 13:39:17 +0900

    BE_220929_test : createChallenge (수정 중)

--- backend/src/main/java/com/ssafy/kirin/service/ChallengeServiceImpl.java
+++ backend/src/main/java/com/ssafy/kirin/service/ChallengeServiceImpl.java
@@ -221,6 +221,12 @@ public class ChallengeServiceImpl implements ChallengeService {
             String mp4File = UUID.randomUUID() + ".mp4";
             System.out.println("aaaaaaaaaaaaaaaaaaaaaa\n"+mp4File);
             p=Runtime.getRuntime().exec(String.format("ffmpeg -y -i %s %s",videoTmpDir,(challengeDir+mp4File)));
+            p.waitFor();
+
+            String outputPath = UUID.randomUUID() + ".mp4";
+            String commandInsertWatermark = String.format("ffmpeg -y -i %s -i %s -i %s -filter_complex \"[1][0]scale2ref=w=oh*mdar:h=ih*0.08[logo][video];[logo]format=argb,geq=r='r(X,Y)':a='0.8*alpha(X,Y)'[soo];[video][soo]overlay=30:30\" -map \"v\" -map 2:a -c:v libx264 -crf 17 -c:a aac -strict experimental %s"
+                    , (challengeDir+mp4File), kirinStamp, musicPath, (challengeDir+outputPath));
+            p= Runtime.getRuntime().exec(commandInsertWatermark);
             String line;
             StringBuilder sb = new StringBuilder();
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -228,13 +234,6 @@ public class ChallengeServiceImpl implements ChallengeService {
             br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             while ((line=br.readLine())!=null) sb.append(line+"\n");
             System.out.println(sb.toString());
-            p.waitFor();
-            String outputPath = UUID.randomUUID() + ".mp4";
-            String commandInsertWatermark = String.format("ffmpeg -y -i %s -i %s -i %s -filter_complex \"[1][0]scale2ref=w=oh*mdar:h=ih*0.08[logo][video];[logo]format=argb,geq=r='r(X,Y)':a='0.8*alpha(X,Y)'[soo];[video][soo]overlay=30:30\" -map \"v\" -map 2:a -c:v libx264 -crf 17 -c:a aac -strict experimental %s"
-                    , (challengeDir+mp4File), kirinStamp, musicPath, (challengeDir+outputPath));
-            p= Runtime.getRuntime().exec(commandInsertWatermark);
-            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((line=br.readLine())!=null) System.out.println(line);
             p.waitFor();
             System.out.println("saving challenge");
             challengeRepository.save(
