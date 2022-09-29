13da3cb27531143e137c502d8d488134853ab91e
류현수 <neillie23@gmail.com>
2022-09-29 13:27:44 +0900

    BE_220929_test : createChallenge (수정 중)

--- backend/src/main/java/com/ssafy/kirin/service/ChallengeServiceImpl.java
+++ backend/src/main/java/com/ssafy/kirin/service/ChallengeServiceImpl.java
@@ -222,8 +222,11 @@ public class ChallengeServiceImpl implements ChallengeService {
             System.out.println("aaaaaaaaaaaaaaaaaaaaaa\n"+mp4File);
             p=Runtime.getRuntime().exec(String.format("ffmpeg -y -i %s %s",videoTmpDir,(challengeDir+mp4File)));
             String line;
+            StringBuilder sb = new StringBuilder();
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((line=br.readLine())!=null) System.out.println(line);
+            while ((line=br.readLine())!=null) sb.append(line);
+            br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while ((line=br.readLine())!=null) sb.append(line);
             p.waitFor();
             String outputPath = UUID.randomUUID() + ".mp4";
             String commandInsertWatermark = String.format("ffmpeg -y -i %s -i %s -i %s -filter_complex \"[1][0]scale2ref=w=oh*mdar:h=ih*0.08[logo][video];[logo]format=argb,geq=r='r(X,Y)':a='0.8*alpha(X,Y)'[soo];[video][soo]overlay=30:30\" -map \"v\" -map 2:a -c:v libx264 -crf 17 -c:a aac -strict experimental %s"
