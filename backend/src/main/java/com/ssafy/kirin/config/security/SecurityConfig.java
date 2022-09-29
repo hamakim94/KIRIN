abca9f067ae9900c5e46bb474b8933acbf7ba3eb
이승연 <seongyeon38@gmail.com>
2022-09-29 23:35:20 +0900

    BE_220929_securityConfig: 비밀번호 변경 api security 인증 필요 없도록 수정 완료

--- backend/src/main/java/com/ssafy/kirin/config/security/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/kirin/config/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
 
     private static final String[] PUBLIC_API_URI = {
-            "/api/notify/**","/api/challenges","/api/users/signup", "/api/users/login", "/api/users/reissue", "/api/users/duplications/check", "/api/users/confirm-email", "api/users/find-password", "/api/users/check-duplicate/**"
+            "/api/notify/**","/api/challenges","/api/users/signup", "/api/users/login", "/api/users/reissue", "/api/users/duplications/check", "/api/users/confirm-email", "/api/users/find-password", "/api/users/check-duplicate/**"
     };
 
     private static final String[] PUBLIC_WEB_URI = {
