a8a74e5036e0fcc0962ec4a6520d1c854149c0cf
이보나 <bona17@naver.com>
2022-09-30 22:16:00 +0900

    FE_220930_donations: API 연동중

--- frontend/src/components/donation/DonationList.js
+++ frontend/src/components/donation/DonationList.js
@@ -1,16 +1,17 @@
-import Donation from "./Donation.json";
-import { Stack, Pagination, Divider } from "@mui/material";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Donation from './Donation.json';
+import { Stack, Pagination, Divider } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useEffect, useState } from 'react';
+import UseAxios from '../../utils/UseAxios';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FFC947",
+      main: '#FFC947',
     },
     secondary: {
-      main: "#11cb5f",
+      main: '#11cb5f',
     },
   },
 });
@@ -29,11 +30,19 @@ function ProgressBar(props) {
 
 function DonationList(props) {
   const { pathname } = useLocation();
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    UseAxios.get(`/challenges?scope=all&order=latest&userid={userid}`, {}).then((res) => {
+      setDonations(res.data);
+      console.log(res);
+    });
+  }, []);
 
   const LAST_PAGE =
-    Donation.donationLists.length % 5 === 0
-      ? parseInt(Donation.donationLists.length / 5)
-      : parseInt(Donation.donationLists.length / 5) + 1; // 마지막 페이지
+    donations.length % 5 === 0
+      ? parseInt(donations.length / 5)
+      : parseInt(donations.length / 5) + 1; // 마지막 페이지
   const [page, setPage] = useState(1); // 처음 페이지는 1이다.
   const [data, setData] = useState([]);
 
@@ -42,9 +51,9 @@ function DonationList(props) {
     // 한 페이지에 5개씩 보여준다.
     if (page === LAST_PAGE) {
       // 마지막 페이지는 데이터가 5개보다 부족할 수도 있다.
-      setData(Donation.donationLists.slice(5 * (page - 1)));
+      setData(donations.slice(5 * (page - 1)));
     } else {
-      setData(Donation.donationLists.slice(5 * (page - 1), 5 * (page - 1) + 5));
+      setData(donations.slice(5 * (page - 1), 5 * (page - 1) + 5));
     }
   }, [page]);
 
@@ -56,59 +65,68 @@ function DonationList(props) {
   return (
     <ThemeProvider theme={theme}>
       <Stack alignItems="center">
-        <Stack direction="row" justifyContent="center" sx={{ flexWrap: "wrap" }}>
-          {data.map((donationList) => {
-            return (
-              <Stack>
-                <hr style={{ width: "100%", marginBottom: 7.5, marginTop: 7.5 }}></hr>
-                <div className={props.styles.donationBox}>
-                  <div className={props.styles.profileImgName}>
-                    <img src={donationList.img} className={props.styles.starImg}></img>
-                    <div className={props.styles.starName}>{donationList.name}</div>
-                  </div>
-                  <div>
-                    <div className={props.styles.participateBox}>
-                      <div className={props.styles.participateChallenge}>
-                        {" "}
-                        <span>챌린지 참여</span>
-                      </div>
-                      {donationList.didDonate === 1 ? (
-                        <div className={props.styles.participateDonation}>기부참여</div>
-                      ) : null}
-                    </div>
-                    <div className={props.styles.challengeTitle}>{donationList.title}</div>
-                    {donationList.didDonate === 1 ? (
-                      <div className={props.styles.donateAmount}>
-                        {donationList.donateAmount}원?
-                      </div>
-                    ) : (
-                      <div className={props.styles.donateAmountZero}>쿠쿠루삥뽕</div>
-                    )}
-                    <div className={props.styles.progressBox}>
-                      <ProgressBar styles={props.styles} width={200} percent={0.7}></ProgressBar>
-                      {donationList.ing === 1 ? (
-                        <div className={props.styles.donateIng}>진행중</div>
-                      ) : (
-                        <div className={props.styles.donateEnd}> 종료</div>
-                      )}
+        <Stack direction="row" justifyContent="center" sx={{ flexWrap: 'wrap' }}>
+          {donations.length === 0 ? (
+            <div style={{ height: '50px', marginLeft: '10px', marginTop: '20PX' }}>
+              참여한 챌린지가 없습니다.
+            </div>
+          ) : (
+            donations.map((donation) => {
+              return (
+                <Stack>
+                  <hr style={{ width: '100%', marginBottom: 7.5, marginTop: 7.5 }}></hr>
+                  <div className={props.styles.donationBox}>
+                    <div className={props.styles.profileImgName}>
+                      <img src={donation.profileImg} className={props.styles.starImg}></img>
+                      <div className={props.styles.starName}>{donation.nickname}</div>
                     </div>
                     <div>
-                      <div className={props.styles.infoBot}>
-                        <span className={props.styles.donateNumber}>
-                          {donationList.donateNum}명
-                        </span>
-                        <span className={props.styles.donatePercent}>
-                          {donationList.donateNum}/{donationList.targetNum}%
-                        </span>
+                      <div className={props.styles.participateBox}>
+                        <div className={props.styles.participateChallenge}>
+                          {' '}
+                          <span>챌린지 참여</span>
+                        </div>
+                        {donation.didDonate === 1 ? (
+                          <div className={props.styles.participateDonation}>기부참여</div>
+                        ) : null}
+                      </div>
+                      <div className={props.styles.challengeTitle}>{donation.title}</div>
+                      {donation.didDonate === 1 ? (
+                        <div className={props.styles.donateAmount}>{donation.donateAmount}KRT</div>
+                      ) : (
+                        <div className={props.styles.donateAmountZero}>쿠쿠루삥뽕</div>
+                      )}
+                      <div className={props.styles.progressBox}>
+                        <ProgressBar
+                          styles={props.styles}
+                          width={200}
+                          percent={donation.currentNum / donation.targetNum}
+                        ></ProgressBar>
+                        {donation.isProceeding === true ? (
+                          <div className={props.styles.donateIng}>진행중</div>
+                        ) : (
+                          <div className={props.styles.donateEnd}> 종료</div>
+                        )}
+                      </div>
+                      <div>
+                        <div className={props.styles.infoBot}>
+                          <span className={props.styles.donateNumber}>{donation.currentNum}명</span>
+                          <span className={props.styles.donatePercent}>
+                            {donation.currentNum / donation.targetNum}%
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </Stack>
-            );
-          })}
+                </Stack>
+              );
+            })
+          )}
+          <div style={{ height: '50px', marginLeft: '10px', marginTop: '20PX' }}>
+            구독한 스타가 없습니다.
+          </div>
         </Stack>
-        <hr style={{ width: "94%", marginBottom: 7.5, marginTop: 7.5 }}></hr>
+        <hr style={{ width: '94%', marginBottom: 7.5, marginTop: 7.5 }}></hr>
         <Pagination
           count={LAST_PAGE}
           defaultPage={1}
