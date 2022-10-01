e45aea69d9c91f063f543ef5e61214122d3c7a2f
류현수 <neillie23@gmail.com>
2022-10-01 19:35:16 +0900

    BE_220930_test : createChallenge (수정 완료)

--- backend/src/main/java/com/ssafy/kirin/service/ChallengeServiceImpl.java
+++ backend/src/main/java/com/ssafy/kirin/service/ChallengeServiceImpl.java
@@ -320,11 +320,6 @@ public class ChallengeServiceImpl implements ChallengeService {
             // wegM to MP4
             String mp4File = UUID.randomUUID() + ".mp4";
             p=Runtime.getRuntime().exec(String.format("ffmpeg -y -i %s %s",videoTmpDir,(challengeDir+mp4File)));
-            BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-            String line;
-            while ((line=br.readLine())!=null) {
-                sb.append(line+"\n");
-            }
             p.waitFor();
             // insert Watermark
 //            String watermarkedVideo = UUID.randomUUID() + ".mp4";
@@ -343,15 +338,9 @@ public class ChallengeServiceImpl implements ChallengeService {
             // insert music
             String outputPath = UUID.randomUUID() + ".mp4";
             String commandInsertMusic = String.format("ffmpeg -y -i %s -i %s -map 0:v -map 1:a -c:v copy -shortest %s",
-                    (challengeDir+mp4File),musicPath,(challengeDir+outputPath));
+                    (challengeDir+mp4File),(challengeDir+musicPath),(challengeDir+outputPath));
             p = Runtime.getRuntime().exec(commandInsertMusic);
-            br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-            while ((line=br.readLine())!=null) {
-                sb.append(line+"\n");
-            }
             p.waitFor();
-            sb.append(LocalDateTime.now());
-            System.out.println(sb);
 //            String commandInsertWatermark = String.format("ffmpeg -y -i %s -i %s -i %s -filter_complex \"[1][0]scale2ref=w=oh*mdar:h=ih*0.08[logo][video];[logo]format=argb,geq=r='r(X,Y)':a='0.8*alpha(X,Y)'[soo];[video][soo]overlay=30:30\" -map \"v\" -map 2:a -c:v libx264 -crf 17 -c:a aac -strict experimental %s"
 //                    , (challengeDir+mp4File), kirinStamp, musicPath, (challengeDir+outputPath));
 
