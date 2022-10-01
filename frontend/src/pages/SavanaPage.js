fd8f53e0474e5a7b5bf4c1a396786cd21134e093
김민균 <mingyun0324@gmail.com>
2022-10-02 03:40:17 +0900

    FE_221002_savana : 사바나 한 페이지씩 이동

--- frontend/src/pages/SavanaPage.js
+++ frontend/src/pages/SavanaPage.js
@@ -5,15 +5,42 @@ import ChallengeList from '../components/savana/ChallengeList';
 
 function SavanaPage() {
   //랜덤으로 바꾸기
+  const [currentHeight, setCurrentHeight] = useState(0);
+  const [touchPosition, setTouchPosition] = useState(null);
   const [popularityData, setPopularityData] = useState(null);
-
+  const [heightArr, setHeightArr] = useState([]);
+  const [idx, setIdx] = useState(0);
+  const windowHeight = window.innerHeight - 56;
+  // 0부터 시작해서 더해지는 배열을 만들자
+  const touchEnd = (e) => {
+    const distanceY = touchPosition.y - e.changedTouches[0].pageY;
+    if (distanceY > 0 && idx < heightArr.length - 1) {
+      window.scrollTo(0, heightArr[idx + 1]);
+      setIdx(idx + 1);
+    } else if (distanceY < 0 && idx > 0) {
+      window.scrollTo(0, heightArr[idx - 1]);
+      setIdx(idx - 1);
+    }
+  };
   useEffect(() => {
     UseAxios.get('/challenges?scope=stars&order=popularity').then((res) => {
       setPopularityData(res.data);
+      for (let i = 0; i < res.data.length; i++) {
+        setHeightArr([...heightArr, i * windowHeight]);
+      }
     });
   }, []);
   return (
-    <div>
+    <div
+      id='savana'
+      onTouchStart={(e) =>
+        setTouchPosition({
+          x: e.changedTouches[0].pageX,
+          y: e.changedTouches[0].pageY,
+        })
+      }
+      onTouchEnd={touchEnd}
+    >
       <ChallengeList styles={styles} data={popularityData}></ChallengeList>
     </div>
   );
