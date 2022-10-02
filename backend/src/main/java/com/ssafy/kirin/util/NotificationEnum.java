6257a508789d381c198f608e6eac17b557e303b8
류현수 <neillie23@gmail.com>
2022-10-02 23:39:55 +0900

    BE_221002_fix : NotificationEnum.java 오타 수정 (완료)

--- backend/src/main/java/com/ssafy/kirin/util/NotificationEnum.java
+++ backend/src/main/java/com/ssafy/kirin/util/NotificationEnum.java
@@ -10,7 +10,7 @@ public enum NotificationEnum {
     CommentReplied("CommentReplied","%s님이 대댓글을 달았습니다."),
     ChallengeCommentAdded("CommentAdded","%s 챌린지에 %s님이 댓글을 달았습니다."),
     CommunityUpload("CommunityUpload","%s님이 커뮤니티를 등록했습니다."),
-    ChallengeUploadCompleted("ChallengeUploadCompleted", "% 챌린지가 정상적으로 업로드 되었습니다.");
+    ChallengeUploadCompleted("ChallengeUploadCompleted", "%s 챌린지가 정상적으로 업로드 되었습니다.");
 
     private final String category;
     private final String content;
