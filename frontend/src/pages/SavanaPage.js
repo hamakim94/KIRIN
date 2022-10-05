00077a724b42a751e2e98d4acd39032de807cad4
이도엽 <lx460@naver.com>
2022-10-05 23:17:31 +0900

    FE_221005_fix: 토큰 버그 수정 및 스크롤 수정

--- frontend/src/pages/SavanaPage.js
+++ frontend/src/pages/SavanaPage.js
@@ -18,73 +18,93 @@ function SavanaPage() {
     if (location)
       switch (pathname[2]) {
         case '0':
-          UseAxios.get(`/challenges?scope=stars&order=random`).then((res) => {
-            setSavanaData(res.data);
-            setIdx(0);
-            setLoading(true);
-          });
+          UseAxios.get(`/challenges?scope=stars&order=random`)
+            .then((res) => {
+              setSavanaData(res.data);
+              setIdx(0);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
         case '1':
-          UseAxios.get(`/challenges?scope=stars&order=popularity`).then((res) => {
-            setSavanaData(res.data);
-            let idx = res.data.findIndex((el) => el.id === location.state.id);
-            setIdx(idx);
-            setLoading(true);
-          });
+          UseAxios.get(`/challenges?scope=stars&order=popularity`)
+            .then((res) => {
+              const arr = res.data;
+              arr.sort(function (a, b) {
+                return b.likeCnt - a.likeCnt;
+              });
+              setSavanaData(arr);
+              let idx = arr.findIndex((el) => el.id === location.state.id);
+              setIdx(idx);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
         case '2':
-          UseAxios.get(`/challenges?scope=stars&order=latest`).then((res) => {
-            setSavanaData(res.data);
-            let idx = res.data.findIndex((el) => el.id === location.state.id);
-            setIdx(idx);
-            setLoading(true);
-          });
+          UseAxios.get(`/challenges?scope=stars&order=latest`)
+            .then((res) => {
+              setSavanaData(res.data);
+              let idx = res.data.findIndex((el) => el.id === location.state.id);
+              setIdx(idx);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
         case '3':
-          UseAxios.get(`/challenges?scope=general&order=random`).then((res) => {
-            const newArr = res.data;
-            let idx = newArr.findIndex((el) => el.id === location.state.id);
-            let firstObj = newArr.splice(idx, 1);
-            newArr.unshift(firstObj[0]);
-            setSavanaData(newArr);
-            setIdx(0);
-            setLoading(true);
-          });
+          UseAxios.get(`/challenges?scope=general&order=random`)
+            .then((res) => {
+              const newArr = res.data;
+              let idx = newArr.findIndex((el) => el.id === location.state.id);
+              let firstObj = newArr.splice(idx, 1);
+              newArr.unshift(firstObj[0]);
+              setSavanaData(newArr);
+              setIdx(0);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
         case '4':
           UseAxios.get(`/challenges?scope=all&order=latest`, {
             params: { challengeId: location.state.challengeId },
-          }).then((res) => {
-            setSavanaData(res.data);
-            let idx = res.data.findIndex((el) => el.id === location.state.id);
-            setIdx(idx);
-            setLoading(true);
-          });
+          })
+            .then((res) => {
+              setSavanaData(res.data);
+              let idx = res.data.findIndex((el) => el.id === location.state.id);
+              setIdx(idx);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
         case '5':
-          UseAxios.get(`/challenges?scope=all&order=latest&userId=${userData.id}`).then((res) => {
-            setSavanaData(res.data);
-            let idx = res.data.findIndex((el) => el.id === location.state.id);
-            setIdx(idx);
-            setLoading(true);
-          });
+          UseAxios.get(`/challenges?scope=all&order=latest&userId=${userData.id}`)
+            .then((res) => {
+              setSavanaData(res.data);
+              let idx = res.data.findIndex((el) => el.id === location.state.id);
+              setIdx(idx);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
         case '6':
-          UseAxios.get(`/challenges/user/${userData.id}`).then((res) => {
-            setSavanaData(res.data);
-            let idx = res.data.findIndex((el) => el.id === location.state.id);
-            setIdx(idx);
-            setLoading(true);
-          });
+          UseAxios.get(`/challenges/user/${userData.id}`)
+            .then((res) => {
+              setSavanaData(res.data);
+              let idx = res.data.findIndex((el) => el.id === location.state.id);
+              setIdx(idx);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
         case 'challenge':
-          UseAxios.get(`/challenges/savana/challenge/${pathname[3]}`).then((res) => {
-            const arr = [];
-            arr.push(res.data);
-            setSavanaData(arr);
-            setIdx(0);
-            setLoading(true);
-          });
+          UseAxios.get(`/challenges/savana/challenge/${pathname[3]}`)
+            .then((res) => {
+              const arr = [];
+              arr.push(res.data);
+              setSavanaData(arr);
+              setIdx(0);
+              setLoading(true);
+            })
+            .catch(() => setLoading(true));
           break;
       }
   }, [location]);
