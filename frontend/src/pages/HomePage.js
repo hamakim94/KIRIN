56eff867bda27471abcf005f2b6cdafc6af05e92
이승연 <seongyeon38@gmail.com>
2022-10-06 15:19:43 +0900

    FE_221006_home : home img 변경 완료

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,6 +5,8 @@ import Category from '../components/common/Category';
 import ChallengeList from '../components/home/ChallengeList';
 import UseAxios from '../utils/UseAxios';
 import NewLoading from '../components/common/NewLoading';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function HomePage() {
   const [popularityData, setPopularityData] = useState(null);
@@ -56,10 +58,21 @@ function HomePage() {
       {/* <hr style={{ border: 'solid 0.1px #C9C9C9' }} /> */}
       <Category title={'인기순'}></Category>
       <ChallengeList styles={styles} data={popularityData} category={1}></ChallengeList>
-      <img className={styles.img} alt='함께' src={require('../assets/img/together.png')}></img>
+      {/* <Carousel autoPlay infiniteLoop showArrows={false} showThumbs={false} showStatus={false}>
+        <div>
+          <img width='100%' src={require('../assets/img/ssafmygirl2.png')} />
+        </div>
+        <div>
+          <img width='100%' src={require('../assets/img/ssafmygirl2.png')} />
+        </div>
+        <div>
+          <img width='100%' src={require('../assets/img/ssafmygirl2.png')} />
+        </div>
+      </Carousel> */}
+      <img className={styles.img} alt='함께' src={require('../assets/img/ssafmygirl2.png')}></img>
       <Category title={'최신순'}></Category>
       <ChallengeList styles={styles} data={latestData} category={2}></ChallengeList>
-      <img className={styles.img} alt='함께' src={require('../assets/img/together.png')}></img>
+      <img className={styles.img} alt='함께' src={require('../assets/img/blockchain3.png')}></img>
       <Category title={'기린기린'}></Category>
       <ChallengeList styles={styles} data={randomData} category={3}></ChallengeList>
     </div>
